dee17dd159403c30810da7de53c54e043f64f970
Afjal Hossain <32141447+Afjal13@users.noreply.github.com>
2020-03-10 20:14:35 +0600

    Add files via upload

---
+++ BusinessPlex/Global.asax.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using BusinessPlex.Models;
+using BusinessPlex.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace BusinessPlex
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initialize AutoMapper
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<UserViewModel, User>();
+                cfg.CreateMap<User, UserViewModel>();
+
+                cfg.CreateMap<CategoryViewModel, Category>();
+                cfg.CreateMap<Category, CategoryViewModel>();
+
+                cfg.CreateMap<ProductViewModel, Product>();
+                cfg.CreateMap<Product, ProductViewModel>();
+
+                cfg.CreateMap<CustomerViewModel, Customer>();
+                cfg.CreateMap<Customer, CustomerViewModel>();
+
+                cfg.CreateMap<SupplierViewModel, Supplier>();
+                cfg.CreateMap<Supplier, SupplierViewModel>();
+            });
+        }
+    }
+}
