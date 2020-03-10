3b9da40d96f5ad8d69a925a21e62430bb9fae70f
Afjal Hossain <32141447+Afjal13@users.noreply.github.com>
2020-03-10 22:12:18 +0600

    Add files via upload

---
+++ BusinessPlex.DatabaseContext/DatabaseContext/BusinessPlexDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BusinessPlex.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessPlex.DatabaseContext.DatabaseContext
+{
+    public class BusinessPlexDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { set; get; }
+        public DbSet<Product> Products { set; get; }
+        public DbSet<Customer> Customers { set; get; }
+        public DbSet<Supplier> Suppliers { set; get; }
+        public DbSet<PurchaseSupplier> PurchaseSuppliers { get; set; }
+        public DbSet<PurchaseDetails> PurchaseDetails { get; set; }
+        public DbSet<SalesCustomer> SalesCustomers { get; set; }
+        public DbSet<SalesDetails> SalesDetails { get; set; }
+    }
+}
